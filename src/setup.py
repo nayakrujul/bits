c66680c1b41e61003fab721bab441b38ca5bda21
Rujul Nayak <55329600+nayakrujul@users.noreply.github.com>
2022-10-26 12:28:06 +0100

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -4,7 +4,7 @@ long_description = 'A very annoying language - read the docs at https://www.gith
 
 setup(
   name = 'bits-lang',
-  version = '1.4',
+  version = '1.5',
   license='Apache',
   description = 'A very annoying language',
   author = 'Rujul Nayak',
